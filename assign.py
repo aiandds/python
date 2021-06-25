33a754a4a7621237815356a516d28d769d326e14
AI & DS <76870716+aiandds@users.noreply.github.com>
2021-06-25 15:12:38 +0530

    Update assign.py

--- assign.py
+++ assign.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 from sklearn import datasets 
 import matplotlib.pyplot as plt
-
+import matplotlib as mpl
 wine = datasets.load_wine()
 print(dir(wine))
 
@@ -9,9 +9,10 @@ print(wine.feature_names)
 
 df = pd.DataFrame(wine.data, columns = wine.feature_names)
 print(df)
-
+df.drop(df.iloc[:, 3:], inplace = True, axis = 1)
+print(wine.target_names)
 df['target'] = wine.target
-print(df)
+print("fnl\n",df)
 
 #df0 = df[:59]
 df0 = df[df['target']==0]
@@ -20,20 +21,27 @@ df1 = df[df['target']==1]
 #print(df1)
 df2 = df[df['target']==2]
 print(df2)
-
+'''
 plt.xlabel('Alcohol')
 plt.ylabel('Malic Acid')
 plt.title('Wine Recognition')
 plt.scatter(df0['alcohol'], df0['malic_acid'],color="green",marker='*')
 plt.scatter(df1['alcohol'], df1['malic_acid'],color="tomato",marker='*')
 plt.show()
-
+'''
+#mpl.rcParams['legend.fontsize'] = 10
 ax = plt.axes(projection='3d')
-ax.scatter3D(df0['alcohol'], df0['malic_acid'], df0['proline'], 'green',marker='*')
-ax.scatter3D(df1['alcohol'], df1['malic_acid'], df1['proline'], 'lightblue',marker='*')
+ax.scatter3D(df0['alcohol'], df0['malic_acid'], df0['ash'], 'green',marker='*')
+ax.scatter3D(df1['alcohol'], df1['malic_acid'], df1['ash'], 'lightblue',marker='*')
+
+ax.legend()
+
+
 ax.set_xlabel('Alcohol')
 ax.set_ylabel('Malic Acid')
 ax.set_zlabel('proline')
 plt.title('Wine Recognition')
-#ax.plot3D(df0['alcohol'], df0['malic_acid'], df0['proline'],'lightgreen')
+
+#ax.plot3D(df0['alcohol'], df0['malic_acid'], df0['ash'],'lightgreen')
+
 plt.show()
